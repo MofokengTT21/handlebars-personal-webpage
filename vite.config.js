f49063169169cecfb8168d01e50185633e33715a
Mofokeng21 <tseko9164@gmail.com>
2024-06-06 23:36:25 +0200

    Debugging

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
 import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite';
+import data from './data.json';
 
-const basicContext = {
-        title : "Hello this is some text",
-        show : false,
-        fruits : [
-            "apples",
-            "oranges",
-            "bananas"
-        ],
-        isClient : true,
-        fruitSales : [
-            {day : "Monday", apples : 13, oranges : 34},
-            {day : "Tuesday", apples : 17, oranges : 28},
-            {day : "Thursday", apples : 7, oranges : 45},
-        ]
-    };
-
-export default {
-  plugins: [handlebars({
-    context : basicContext
+export default defineConfig({
+    base : '/responsive_webpages/',
+    build: {
+        rollupOptions: {
+            input : {
+                main: 'index.html',
+            }
+        },
+    },
+    plugins: [handlebars({
+      context : {
+        data
+      }
   })]
-};
+    
+});
