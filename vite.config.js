7768fae1a7bdb5dfe6bc64a14544a09438c28d7b
Mofokeng21 <tseko9164@gmail.com>
2024-06-07 01:31:01 +0200

    Fixed handlebars

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,11 @@ import handlebars from 'vite-plugin-handlebars';
 import { defineConfig } from 'vite';
 import data from './data.json';
 
-export default defineConfig({
-    base : '/responsive_webpages/',
-    build: {
-        rollupOptions: {
-            input : {
-                main: 'index.html',
-            }
-        },
-    },
-    plugins: [handlebars({
-      context : {
-        data
-      }
-  })]
-    
-});
+export default {
+  base : '/responsive_webpages/',   // <-- add this
+  plugins: [
+    handlebars({
+      context: data, 
+    }),
+  ],
+};
